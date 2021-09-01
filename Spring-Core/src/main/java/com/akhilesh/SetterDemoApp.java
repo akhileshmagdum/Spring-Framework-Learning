978903ace73437246ef069dffa9a1af9f173edf6
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-01 22:51:32 +0530

    Edited git errors

--- Spring-Core/src/main/java/com/akhilesh/SetterDemoApp.java
+++ Spring-Core/src/main/java/com/akhilesh/SetterDemoApp.java
@@ -7,12 +7,8 @@ public class SetterDemoApp {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         CricketCoach cricketCoach = context.getBean("myCricketCoach",CricketCoach.class);
-<<<<<<< HEAD
         System.out.println(cricketCoach.getDailyFortune()+" "+cricketCoach.getDailyWorkout()+" "+ cricketCoach.getTeamName());
-
-=======
         System.out.println(cricketCoach.getDailyFortune()+" "+cricketCoach.getDailyWorkout());
->>>>>>> 1a230a9c37a04d6579ca1e691d6048ce70ef2c4d
         context.close();
     }
 }
