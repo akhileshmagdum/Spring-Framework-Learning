66fe8d18551cc97d38cb71f1f28ab8ad0a011778
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-03 12:23:28 +0530

    Comment on changed class

--- Spring-Core-NoXml/src/main/java/com/akhilesh/App.java
+++ Spring-Core-NoXml/src/main/java/com/akhilesh/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
 
+        //changed because using class method and not interface method
         RunCoach theCoach = applicationContext.getBean("runner",RunCoach.class);
         System.out.println(theCoach.getWorkout()+"\n"+theCoach.getFortuneService()+"\n \t"+theCoach.getName());
     }
