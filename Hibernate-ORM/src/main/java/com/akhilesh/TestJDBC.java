02cb8e97bc4d0d8b20866466f014f67336c5f491
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-06 13:41:58 +0530

    Hibernate Configuration

--- Hibernate-ORM/src/main/java/com/akhilesh/TestJDBC.java
+++ Hibernate-ORM/src/main/java/com/akhilesh/TestJDBC.java
@@ -7,11 +7,11 @@ import java.sql.SQLException;
 public class TestJDBC {
     public static void main(String[] args) {
         String jdbcurl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
-        String username ="hbstudent";
+        String usernamePassword ="hbstudent";
 
 
         try {
-            Connection connection = DriverManager.getConnection(jdbcurl,username,username);
+            Connection connection = DriverManager.getConnection(jdbcurl,usernamePassword,usernamePassword);
             System.out.println("Connection established");
         } catch (SQLException e) {
             e.printStackTrace();
