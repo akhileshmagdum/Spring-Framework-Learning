40237b918ead5bc37fb703294a3aa9db0dac110d
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-06 13:08:38 +0530

    JDBC connection established

---
+++ Hibernate-ORM/src/main/java/com/akhilesh/TestJDBC.java
@@ -0,0 +1,20 @@
+package com.akhilesh;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestJDBC {
+    public static void main(String[] args) {
+        String jdbcurl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+        String username ="hbstudent";
+
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcurl,username,username);
+            System.out.println("Connection established");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
