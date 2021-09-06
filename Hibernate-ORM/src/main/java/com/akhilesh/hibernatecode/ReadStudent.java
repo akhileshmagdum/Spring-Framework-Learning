91c0fa4acc1debff411d5b61e3b98840e846aaa0
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-06 17:58:02 +0530

    Read Values in DB

--- Hibernate-ORM/src/main/java/com/akhilesh/hibernatecode/ReadStudent.java
+++ Hibernate-ORM/src/main/java/com/akhilesh/hibernatecode/ReadStudent.java
@@ -4,23 +4,27 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-
-public class CreateStudent {
+public class ReadStudent {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Student.class)
                 .buildSessionFactory();
-        Session session = factory.getCurrentSession();
 
+        Session session = factory.getCurrentSession();
         try {
-            Student student = new Student("Akhilesh","Magdum","akhilesh@duck.com");
+            Student student = new Student("Vijay","Sharma","vijay@sharma.com");
             session.beginTransaction();
             session.save(student);
             session.getTransaction().commit();
-        }
-        finally {
-            factory.close();
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            System.out.println(" "+student.getId());
+            Student getStudentFromDB = session.get(Student.class,student.getId());
+            System.out.println(getStudentFromDB);
+            session.getTransaction().commit();
+        }finally {
+
         }
     }
 }
