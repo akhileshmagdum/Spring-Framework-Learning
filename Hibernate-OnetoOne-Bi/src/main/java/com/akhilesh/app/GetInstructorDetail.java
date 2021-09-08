57c11c01944947b61141a608ee615fae003708f3
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-08 18:21:10 +0530

    OneToOne Bidirectional

---
+++ Hibernate-OnetoOne-Bi/src/main/java/com/akhilesh/app/GetInstructorDetail.java
@@ -0,0 +1,30 @@
+package com.akhilesh.app;
+
+import com.akhilesh.entity.Instructor;
+import com.akhilesh.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetInstructorDetail {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+        try {
+            session.beginTransaction();
+
+            int id = 1;
+            InstructorDetail detail = session.get(InstructorDetail.class,id);
+            System.out.println(detail);
+            //Getting the associated instructor with that detail
+            System.out.println(detail.getInstructor());
+            session.getTransaction().commit();
+        }finally {
+            factory.close();
+        }
+    }
+}
--- Hibernate-OnetoOne-Bi/src/main/java/com/akhilesh/entity/InstructorDetail.java
+++ Hibernate-OnetoOne-Bi/src/main/java/com/akhilesh/entity/InstructorDetail.java
@@ -17,6 +17,18 @@ public class InstructorDetail {
     @Column(name = "hobby")
     private String hobby;
 
+    //here the mappedBy Property is taken from the Instructor class
+    @OneToOne(mappedBy = "instructorDetail",cascade = CascadeType.ALL)
+    private Instructor instructor;
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
     public InstructorDetail(){ }
 
     public InstructorDetail(String youtubeChannel, String hobby) {
