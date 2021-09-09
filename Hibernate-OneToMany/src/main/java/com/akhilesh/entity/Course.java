e1c64b7497164a4671a882194377e1235f517308
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-09 18:42:47 +0530

    OneToMany Create

---
+++ Hibernate-OneToMany/src/main/java/com/akhilesh/entity/Course.java
@@ -0,0 +1,61 @@
+package com.akhilesh.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne(cascade = {CascadeType.MERGE,CascadeType.PERSIST,
+    CascadeType.DETACH,CascadeType.REFRESH})
+    //Here the reference to the instructor id is given so hibernate will know how to find the instructor
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    public Course() { }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", instructor=" + instructor +
+                '}';
+    }
+}
