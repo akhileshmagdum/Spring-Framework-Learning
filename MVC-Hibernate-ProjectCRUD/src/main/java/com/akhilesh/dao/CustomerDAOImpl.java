a1f0f063c23cf7d1b6c3bc3c0a103feb5833ca3b
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-12 20:42:31 +0530

    Added Customer DAO with Customer List

---
+++ MVC-Hibernate-ProjectCRUD/src/main/java/com/akhilesh/dao/CustomerDAOImpl.java
@@ -0,0 +1,30 @@
+package com.akhilesh.dao;
+
+import com.akhilesh.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO{
+
+    //Injecting dependency from the servlet.xml file
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+
+        Session session = sessionFactory.getCurrentSession();
+        Query<Customer> customerQuery = session.createQuery("from Customer",Customer.class);
+        List<Customer> customers = customerQuery.getResultList();
+
+        return customers;
+    }
+}
