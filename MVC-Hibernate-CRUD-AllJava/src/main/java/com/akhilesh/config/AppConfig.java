ef5cd29c9dd4650648f79bc7f9dbd19d04bfb743
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-17 11:31:26 +0530

    Java Config Complete NOT TESTED

--- MVC-Hibernate-CRUD-AllJava/src/main/java/com/akhilesh/config/AppConfig.java
+++ MVC-Hibernate-CRUD-AllJava/src/main/java/com/akhilesh/config/AppConfig.java
@@ -1,19 +1,24 @@
 package com.akhilesh.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 /*
@@ -39,9 +44,127 @@ public class AppConfig implements WebMvcConfigurer {
     @Bean
     public ViewResolver viewResolverConfiguration(){
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-
         resolver.setPrefix("/WEB-INF/view/");
         resolver.setSuffix(".jsp");
         return resolver;
+    }
+
+    /*
+    <bean id="myDataSource" class="com.mchange.v2.c3p0.ComboPooledDataSource"
+          destroy-method="close">
+        <property name="driverClass" value="com.mysql.cj.jdbc.Driver" />
+        <property name="jdbcUrl" value="jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&amp;serverTimezone=UTC" />
+        <property name="user" value="springstudent" />
+        <property name="password" value="springstudent" />
+
+        <!-- these are connection pool properties for C3P0 -->
+        <property name="initialPoolSize" value="5"/>
+        <property name="minPoolSize" value="5" />
+        <property name="maxPoolSize" value="20" />
+        <property name="maxIdleTime" value="30000" />
+    </bean>
+     */
+    @Bean
+    public DataSource myDataSource() {
+
+        // create connection pool
+        ComboPooledDataSource myDataSource = new ComboPooledDataSource();
+
+        // set the jdbc driver
+        try {
+            myDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        }
+        catch (PropertyVetoException exc) {
+            throw new RuntimeException(exc);
+        }
+
+        // Just to make sure we are reading the data
+        logger.info("jdbc.url=" + env.getProperty("jdbc.url"));
+        logger.info("jdbc.user=" + env.getProperty("jdbc.user"));
+
+        // set database connection props
+        myDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        myDataSource.setUser(env.getProperty("jdbc.user"));
+        myDataSource.setPassword(env.getProperty("jdbc.password"));
+
+        // set connection pool props
+        myDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        myDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        myDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        myDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return myDataSource;
+    }
+
+    /*
+    <property name="hibernateProperties">
+            <props>
+                <prop key="hibernate.dialect">org.hibernate.dialect.MySQLDialect</prop>
+                <prop key="hibernate.show_sql">true</prop>
+            </props>
+        </property>
+     */
+    private Properties getHibernateProperties() {
+
+        // set hibernate properties
+        Properties props = new Properties();
+
+        props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+
+        return props;
+    }
+
+    // read environment property and convert to int
+    private int getIntProperty(String propName) {
+
+        String propVal = env.getProperty(propName);
+        int intPropVal = Integer.parseInt(propVal);
+
+        return intPropVal;
+    }
+
+
+    /*
+ <bean id="sessionFactory"
+          class="org.springframework.orm.hibernate5.LocalSessionFactoryBean">
+        <property name="dataSource" ref="myDataSource" />
+        <property name="packagesToScan" value="com.akhilesh.entity" />
+        <hibernate.......>
+    </bean>
+     */
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(){
+
+        // create session factorys
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+        // set the properties
+        sessionFactory.setDataSource(myDataSource());
+        sessionFactory.setPackagesToScan(env.getProperty("hibernate.packagesToScan"));
+        sessionFactory.setHibernateProperties(getHibernateProperties());
+
+        return sessionFactory;
+    }
+
+    /*
+<!-- Step 3: Setup Hibernate transaction manager -->
+    <bean id="myTransactionManager"
+          class="org.springframework.orm.hibernate5.HibernateTransactionManager">
+        <property name="sessionFactory" ref="sessionFactory"/>
+    </bean>
+
+    <!-- Step 4: Enable configuration of transactional behavior based on annotations -->
+    <tx:annotation-driven transaction-manager="myTransactionManager" />
+     */
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+
+        // setup transaction manager based on session factory
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory);
+
+        return txManager;
     }
 }
