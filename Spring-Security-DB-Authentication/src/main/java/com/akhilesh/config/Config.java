7483b9f95fcaa98de08bf88c6a3ac17f4746327f
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-20 18:07:48 +0530

    MVC Setup

---
+++ Spring-Security-DB-Authentication/src/main/java/com/akhilesh/config/Config.java
@@ -0,0 +1,24 @@
+package com.akhilesh.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.akhilesh")
+public class Config {
+
+    @Bean
+    public ViewResolver viewResolver(){
+
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+}
---
+++ Spring-Security-DB-Authentication/src/main/webapp/WEB-INF/view/homepage.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Captain
+  Date: 20-09-2021
+  Time: 05:54 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+
+<h1>Spring Security with Database Authentication</h1>
+
+</body>
+</html>
