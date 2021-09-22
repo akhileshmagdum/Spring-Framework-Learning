aeda270c01f78582331b964bc8f3a8eabd567c1c
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-22 19:49:36 +0530

    Added all customers

--- Rest-CRUD/src/main/java/com/akhilesh/restcontroller/Controller.java
+++ Rest-CRUD/src/main/java/com/akhilesh/restcontroller/Controller.java
@@ -3,9 +3,12 @@ package com.akhilesh.restcontroller;
 import com.akhilesh.entity.Customer;
 import com.akhilesh.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -17,5 +20,10 @@ public class Controller {
     @RequestMapping("/customer/{customerId}")
     public Customer showSingleCustomer(@PathVariable int customerId){
         return customerService.getSingleCustomer(customerId);
+    }
+
+    @GetMapping("/customers")
+    public List<Customer> getAllCustomers(){
+        return customerService.getAllCustomers();
     }
 }
