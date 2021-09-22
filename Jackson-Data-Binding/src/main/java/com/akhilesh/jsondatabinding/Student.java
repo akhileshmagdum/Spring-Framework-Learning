a8cf8ed966d1f91e8c135355dacb9c959e6d238f
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-22 10:52:34 +0530

    Support for unknown properties

--- Jackson-Data-Binding/src/main/java/com/akhilesh/jsondatabinding/Student.java
+++ Jackson-Data-Binding/src/main/java/com/akhilesh/jsondatabinding/Student.java
@@ -1,5 +1,8 @@
 package com.akhilesh.jsondatabinding;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
 
     private int id;
