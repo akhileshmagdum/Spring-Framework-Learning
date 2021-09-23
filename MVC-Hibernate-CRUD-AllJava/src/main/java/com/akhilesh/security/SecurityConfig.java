356352fa1314ace11bb5f4a4c8fcd36c71c1f94e
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-23 23:06:31 +0530

    Added security configuration

---
+++ MVC-Hibernate-CRUD-AllJava/src/main/java/com/akhilesh/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.akhilesh.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource securityDataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(securityDataSource);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/customer/lists").hasAnyRole("EMPLOYEE","MANAGER","ADMIN")
+                .antMatchers("/customer/search").permitAll().antMatchers("customer/delete").hasRole("ADMIN")
+                .and().formLogin().loginPage("/login")
+                .loginProcessingUrl("/auth-user").permitAll();
+    }
+}
