280e835122269cd97200968a6b6ea58db1b083ef
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-23 15:36:31 +0530

    Add customer

--- Rest-CRUD/src/main/java/com/akhilesh/restcontroller/Controller.java
+++ Rest-CRUD/src/main/java/com/akhilesh/restcontroller/Controller.java
@@ -4,10 +4,7 @@ import com.akhilesh.entity.Customer;
 import com.akhilesh.errorhandling.CustomerException;
 import com.akhilesh.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,8 +24,21 @@ public class Controller {
         return customer;
     }
 
-    @GetMapping("/customers")
+    @GetMapping("/customer/all")
     public List<Customer> getAllCustomers(){
         return customerService.getAllCustomers();
+    }
+
+
+    /*
+    https://stackoverflow.com/questions/30511911/getting-not-supported-media-type-error
+     */
+    @PostMapping("/customer")
+    public Customer addCustomer(@RequestBody Customer customer){
+
+        //In DAO we have used save or update method so 0 acts as null and hence a new customer will be added
+        customer.setId(0);
+        customerService.saveCustomer(customer);
+        return customer;
     }
 }
