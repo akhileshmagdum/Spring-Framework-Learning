d7568f0a0d042dc053d5c233bf6dcb2cfb653834
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-24 10:04:36 +0530

    Login page at start

--- MVC-Hibernate-CRUD-AllJava/src/main/java/com/akhilesh/security/SecurityConfig.java
+++ MVC-Hibernate-CRUD-AllJava/src/main/java/com/akhilesh/security/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 import javax.sql.DataSource;
 
@@ -20,13 +22,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(securityDataSource);
     }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll()
-                .antMatchers("/customer/lists").hasAnyRole("EMPLOYEE","MANAGER","ADMIN")
-                .antMatchers("/customer/search").permitAll().antMatchers("customer/delete").hasRole("ADMIN")
-                .and().formLogin().loginPage("/login")
-                .loginProcessingUrl("/auth-user").permitAll();
-    }
+    
 }
