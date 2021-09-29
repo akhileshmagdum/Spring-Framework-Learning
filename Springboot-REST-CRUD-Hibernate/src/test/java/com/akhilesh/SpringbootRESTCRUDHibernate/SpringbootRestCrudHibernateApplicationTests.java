d72c8ae201596efb6cc93609f834f50d186988d6
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-29 11:17:52 +0530

    Project setup

---
+++ Springboot-REST-CRUD-Hibernate/src/test/java/com/akhilesh/SpringbootRESTCRUDHibernate/SpringbootRestCrudHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhilesh.SpringbootRESTCRUDHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestCrudHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
