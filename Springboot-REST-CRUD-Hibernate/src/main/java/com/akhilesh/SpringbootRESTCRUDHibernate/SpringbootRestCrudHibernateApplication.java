c7206e53a6eac1fc94dd3bb721631c2549ac9102
akhileshmagdum <akhileshmagdum@gmail.com>
2021-09-29 11:17:52 +0530

    Project setup

---
+++ Springboot-REST-CRUD-Hibernate/src/main/java/com/akhilesh/SpringbootRESTCRUDHibernate/SpringbootRestCrudHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.akhilesh.SpringbootRESTCRUDHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestCrudHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestCrudHibernateApplication.class, args);
+	}
+
+}
